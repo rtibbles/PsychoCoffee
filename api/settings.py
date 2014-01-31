84da252ace2cdcaf41e3efa02636124b9961c636
Richard <richard.tibbles@gmail.com>
2014-01-30 17:03:36 -0800

    Chaplin boilerplate. First-pass RESTful API using Eve.

---
+++ api/settings.py
@@ -0,0 +1,54 @@
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'user'
+MONGO_PASSWORD = 'user'
+MONGO_DBNAME = 'apitest'
+
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+participants_schema = {
+
+    'firstname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'lastname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        'unique': True,
+    },
+
+    'location': {
+        'type': 'dict',
+        'schema': {
+            'address': {'type': 'string'},
+            'city': {'type': 'string'}
+        },
+    },
+    'born': {
+        'type': 'datetime',
+    },
+}
+
+participants = {
+
+    'item_title': 'participants',
+
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+
+    'schema': participants_schema
+}
+
+SERVER_NAME = '127.0.0.1:5000'
+DOMAIN = {
+    'participants': participants,
+}
